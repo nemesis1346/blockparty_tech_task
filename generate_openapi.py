82ffdf38759dbd1440465cc46af1728c26ca9b2a
Marco Maigua <marco.maigua1346@gmail.com>
2025-04-03 21:31:53 -0400

    add api docs deployment

--- generate_openapi.py
+++ generate_openapi.py
@@ -1,3 +1,8 @@
+# generate_openapi.py
+from fastapi import FastAPI
+from api.main import app  # Import your FastAPI app
+import json
+
 if __name__ == "__main__":
     openapi_schema = app.openapi()
     with open("openapi.json", "w") as f:
